a5dc605e982f7156ecc797b1562e57337e96f277
N3o D4rk3r <61631611+n3o-d4rk3r@users.noreply.github.com>
2021-03-25 00:37:22 +0600

    Add files via upload

---
+++ Android Studio Projects/SpeeedMeterPro/app/src/main/java/com/ipixel/speedmeterpro/activities/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ipixel.speedmeterpro.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ipixel.speedmeterpro.R;
+
+/**
+ * The type Home activity.
+ */
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate (@Nullable Bundle savedInstanceState) {
+		super.onCreate (savedInstanceState);
+		setContentView (R.layout.activity_main);
+	}
+
+
+}
